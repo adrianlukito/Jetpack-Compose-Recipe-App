d68b76930e8dc88cce92c9b766b6608e2648ee3a
adrianlukito <lukito.adrian1@gmail.com>
2021-09-24 12:16:54 +0700

    dto & entity refactor

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/network/RecipeService.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/network/RecipeService.kt
@@ -1,6 +1,6 @@
 package com.codingwithmitch.mvvmrecipeapp.network
 
-import com.codingwithmitch.mvvmrecipeapp.network.model.RecipeNetworkEntity
+import com.codingwithmitch.mvvmrecipeapp.network.model.RecipeDto
 import com.codingwithmitch.mvvmrecipeapp.network.responses.RecipeSearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -19,5 +19,5 @@ interface RecipeService {
     suspend fun get(
         @Header("Authorization") token: String,
         @Query("id") id: Int,
-    ): RecipeNetworkEntity
+    ): RecipeDto
 }
