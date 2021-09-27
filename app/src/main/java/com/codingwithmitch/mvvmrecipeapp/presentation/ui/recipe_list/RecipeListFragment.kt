85ead278cbbe1552bdfa2a995156387cfa68ea17
adrianlukito <lukito.adrian1@gmail.com>
2021-09-27 15:20:05 +0700

    recyclerview & cardview

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
@@ -19,6 +20,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.codingwithmitch.mvvmrecipeapp.R
+import com.codingwithmitch.mvvmrecipeapp.presentation.components.RecipeCard
 import com.codingwithmitch.mvvmrecipeapp.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -41,22 +43,9 @@ class RecipeListFragment: Fragment() {
             setContent {
                 val recipes = viewModel.recipes.value
                 
-                for (recipe in recipes) {
-                    Log.d(TAG, "onCreateView: ${recipe.title}")
-                }
-
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Recipe List",
-                        style = TextStyle(
-                            fontSize = 21.sp
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = {
-                        findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment)
-                    }) {
-                        Text(text = "TO RECIPE FRAGMENT")
+                LazyColumn {
+                    itemsIndexed(items = recipes) { index, recipe ->
+                        RecipeCard(recipe = recipe, onClick = {})
                     }
                 }
             }
